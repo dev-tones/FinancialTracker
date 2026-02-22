fdadb409d0f38d81eb0e70368d55f5fda2b4d36e
Matthew Twigg <dev_tones@outlook.com>
2026-02-22 15:05:39 -0500

    GET and POST mapping created for Transaction

---
+++ backend/src/main/java/com/twigg/backend/controller/TransactionController.java
@@ -0,0 +1,43 @@
+package com.twigg.backend.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import com.twigg.backend.service.TransactionService;
+import com.twigg.backend.dto.CreateTransactionRequest;
+import com.twigg.backend.dto.TransactionResponse;
+
+import java.util.List;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+
+
+@RestController
+@RequestMapping("/transaction")
+public class TransactionController {
+
+    private final TransactionService transactionService;
+    public TransactionController(TransactionService transactionService) {
+        this.transactionService = transactionService;
+    }
+    @PostMapping
+    public TransactionResponse createTransaction(@RequestBody CreateTransactionRequest request) {
+        TransactionResponse response = transactionService.createTransaction(request);
+        return response;
+    }
+    @GetMapping
+    public List<TransactionResponse> getAllTransactions() {
+        List<TransactionResponse> transactionResponse = transactionService.getAllTransactions();
+        return transactionResponse;
+    }
+    
+
+}
+    
+
+
+
+
