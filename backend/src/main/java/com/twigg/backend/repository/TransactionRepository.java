744d94b7788807bbe5394f30d793b45e587c5c30
Matthew Twigg <10098218@id.ohio.gov>
2026-06-30 13:52:59 -0400

    pagination to reoccurring transactions

--- backend/src/main/java/com/twigg/backend/repository/TransactionRepository.java
+++ backend/src/main/java/com/twigg/backend/repository/TransactionRepository.java
@@ -1,13 +1,13 @@
 package com.twigg.backend.repository;
 
-import java.util.List;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.twigg.backend.model.Transaction;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
 
-    public List<Transaction> findByReoccurring(boolean reoccurring);
+    public Page<Transaction> findByReoccurring(boolean reoccurring, Pageable pageable);
 
 }
