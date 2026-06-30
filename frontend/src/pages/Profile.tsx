ca70abd66ebd59150178172f6395f322257df510
Matthew Twigg <dev_tones@outlook.com>
2026-06-29 21:33:53 -0400

    profile fixes

--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -2,9 +2,9 @@ import { useState, useEffect } from "react";
 import type { User } from "../types/User";
 
 function Profile() {
-  const [ loading, setLoading ] = useState(false);
-  const [ error, setError ] = useState("");
-  const [ tableData, setTableData ] = useState<User>({
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+  const [tableData, setTableData] = useState<User>({
     id: 0,
     userRole: "",
     userName: "",
@@ -16,68 +16,66 @@ function Profile() {
     updatedAt: "",
   });
 
-useEffect(() => {
-const fetchUserData = async () => {
-
-  const email = localStorage.getItem("email");
-  if(!email){
-    setError("No email found.");
-    setLoading(false);
-    return;
-  }
-  try {
-    const response = await fetch(`http://localhost:8080/api/v1/search?email=${email}`,
-      {
-        method: "GET",
-        headers: 
-        {
-          "Content-Type": "application/json"
-        }
-      });
-    if(!response.ok){
-      throw new Error(`HTTP Error! Status: ${response.status}`);
-    }
-    const data: User = await response.json();
-    setTableData(data);
-  } catch (err) {
-      if(err instanceof Error){
-        setError(err.message);
-      } else {
-        setError("Something went wrong");
+  useEffect(() => {
+    const fetchUserData = async () => {
+      const email = localStorage.getItem("email");
+      if (!email) {
+        setError("No email found.");
+        setLoading(false);
+        return;
       }
-  } finally {
-    setLoading(false);
-  }
-};
-fetchUserData();
-}, []);
-  
-if(loading) return <p>Loading profile...</p>;
-if(error) return <p style={{ color: 'red' }}>Error: {error}</p>; 
+      try {
+        const response = await fetch(
+          `http://localhost:8080/api/v1/user/search?email=${email}`,
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${localStorage.getItem("token")}`,
+            },
+          },
+        );
+        if (!response.ok) {
+          throw new Error(`HTTP Error! Status: ${response.status}`);
+        }
+        const data: User = await response.json();
+        setTableData(data);
+      } catch (err) {
+        if (err instanceof Error) {
+          setError(err.message);
+        } else {
+          setError("Something went wrong");
+        }
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchUserData();
+  }, []);
+
+  if (loading) return <p>Loading profile...</p>;
+  if (error) return <p style={{ color: "red" }}>Error: {error}</p>;
   return (
     <table>
       <tbody>
-      <tr>
-        Name:
-        <td>
-          {tableData.firstName} {tableData.lastName}
-        </td>
-      </tr>
-      <tr>
-        <td>
-          {tableData.userRole}
-        </td>
-      </tr>
-      <tr>
-        <td>
-          {tableData.email}
-        </td>
-      </tr>
-      <tr>
-        <td>
-          {tableData.phone}
-        </td>
-      </tr>
+        <tr>
+          <th>Name:</th>
+          <td>
+            {tableData.firstName} {tableData.lastName}
+          </td>
+        </tr>
+        <tr>
+          <th>Role:</th>
+          <td> {tableData.userRole}</td>
+        </tr>
+        <tr>
+          <th>Email:</th>
+          <td> {tableData.email}</td>
+        </tr>
+        <tr>
+          <th>Phone:</th>
+          <td> {tableData.phone}</td>
+        </tr>
       </tbody>
     </table>
   );
